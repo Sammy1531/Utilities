7809fd42e12954446a740d63e16ef7aa88cddeba
Soumitra Mukherjee <soumitra.mukherjee@lisec.com>
2020-05-26 13:44:50 +0400

    added LiSecUtilities test

---
+++ LiSECUtil/LiUtilities.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LiSECUtilities
+{
+    public class LiUtilities
+    {
+        public static void PrintString(string value)
+        {
+            for(int i=0; i<value.Length; i++)
+            {
+                Console.WriteLine($"The character at {i} is : {value[i]}");
+            }
+        }
+
+        public static string ToUpperCase(string str)
+        {
+            Console.WriteLine("String entered " + str);
+            return str.ToUpper();
+        }
+
+        public static string ToLowerCase(string str)
+        {
+            Console.WriteLine("String entered " + str);
+            return str.ToLower();
+        }
+    }
+}
